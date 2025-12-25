594d889b22d0f4b571e2cb7e6bffce3a9af6951d
Jwenger007 <brmdgkb2t5@privaterelay.appleid.com>
2025-12-25 14:17:38 +0100

    Update 02modify_tables.sql

--- sql/02modify_tables.sql
+++ sql/02modify_tables.sql
@@ -1,11 +1,5 @@
-\copy online_retail(invoiceno)   FROM 'PASTE_PATH_HERE/online_retail.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
-\copy online_retail(stockcode)   FROM 'PASTE_PATH_HERE/online_retail.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
-\copy online_retail(description) FROM 'PASTE_PATH_HERE/online_retail.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
-\copy online_retail(quantity)    FROM 'PASTE_PATH_HERE/online_retail.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
-\copy online_retail(invoicedate) FROM 'PASTE_PATH_HERE/online_retail.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
-\copy online_retail(unitprice)   FROM 'PASTE_PATH_HERE/online_retail.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
-\copy online_retail(customerid)  FROM 'PASTE_PATH_HERE/online_retail.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
-\copy online_retail(country)     FROM 'PASTE_PATH_HERE/online_retail.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+\copy online_retail FROM '\Users\Johan\Documents\Data_Analyst_skills\SQL_Project_(Luke_SQL_YT)(VS_Code)\csv_files_project2\online_retail.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 */
 
 -- This SQL script was executed using PostgreSQL via pgAdmin 4 (Query Tool / psql)
