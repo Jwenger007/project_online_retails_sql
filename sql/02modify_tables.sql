4f8e74d6e02a7cab20d8526d7d9f21a015c0a817
Jwenger007 <brmdgkb2t5@privaterelay.appleid.com>
2025-12-26 16:52:59 +0100

    Update 02modify_tables.sql

--- sql/02modify_tables.sql
+++ sql/02modify_tables.sql
@@ -1,6 +1,5 @@
 \copy online_retail FROM '\Users\Johan\Documents\Data_Analyst_skills\SQL_Project_(Luke_SQL_YT)(VS_Code)\csv_files_project2\online_retail.csv'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
-*/
 
 -- This SQL script was executed using PostgreSQL via pgAdmin 4 (Query Tool / psql)
 -- Database: online_retail
